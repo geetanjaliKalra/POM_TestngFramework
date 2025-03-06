cb881e0c6f9952cfed048bf24a8c86cdeb3eda79
Geetanjali <gkalra88@gmail.com>
2025-03-06 18:57:30 +0400

    POM folder structure

---
+++ POMTestNg/src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+import com.qa.opencart.base.BaseTest;
+
+public class LoginPageTest extends BaseTest {
+
+	@Test
+	public void test() {
+		
+	}
+}
